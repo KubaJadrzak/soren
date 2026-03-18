50c25f0c02f1bf1b2abec367baf8a939c1a7678e
KubaJadrzak <kuba.jadrzak@gmail.com>
2026-03-18 21:44:50 +0100

    configure app for sorbet

--- lib/soren.rb
+++ lib/soren.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "soren/version"
+require_relative 'soren/version'
 
 module Soren
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    #: (base_url: untyped, ?adapter: untyped) -> Sofia::Client
+    def new(base_url:, port:)
+      true
+    end
+  end
 end
